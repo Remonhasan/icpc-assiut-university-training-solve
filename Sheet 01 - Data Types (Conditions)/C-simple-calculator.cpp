97a98ae999f2ee1a36b86f0eb59d073c9216bc36
remonhasanapu <remon.hasan@technovista.com.bd>
2023-11-28 14:51:43 +0600

    Problem C

---
+++ Sheet 01 - Data Types (Conditions)/C-simple-calculator.cpp
@@ -0,0 +1,13 @@
+/*
+  author: remon hasan 
+*/
+#include<stdio.h>
+
+int main() {
+  long long a, b;
+  scanf("%lld %lld", & a, & b);
+  printf("%lld + %lld = %lld\n", a, b, a + b);
+  printf("%lld * %lld = %lld\n", a, b, a * b);
+  printf("%lld - %lld = %lld\n", a, b, a - b);
+
+}
